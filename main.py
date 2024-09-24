970b413a0191d049f1388cf379e8da29612eb42a
wish99benjamin <wish99benjamin@gmail.com>
2024-09-24 19:05:18 -0400

    Update: add producing balls automatically

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import random
 from PyQt5.QtWidgets import QApplication, QGraphicsView, QGraphicsScene
 from PyQt5.QtGui import QBrush, QColor
+from PyQt5.QtCore import QTimer
 from ball import Ball
 
 class MainWindow(QGraphicsView):
@@ -20,6 +21,12 @@ class MainWindow(QGraphicsView):
         self.setBackgroundBrush(QColor(230, 230, 230))
         self.createScene()
 
+        # init timer
+        self.timeTooEmpty = 0
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.updateScene)
+        self.timer.start(50)
+
     def createScene(self):
         self.scene = QGraphicsScene()
         for num in range(5):
@@ -32,10 +39,25 @@ class MainWindow(QGraphicsView):
         scene_rect = self.mapToScene(view_rect).boundingRect()
         self.scene.setSceneRect(scene_rect)
 
-        
-        
+    def updateScene(self):
+        counter = 0
+        for item in self.scene.items():
+            if isinstance(item, Ball): counter += 1
+            if counter >= 5: 
+                self.timeTooEmpty = 0
+                break
 
+        if counter < 5:
+            self.timeTooEmpty += 1
+            if self.timeTooEmpty >= 60:
+                self.addBall()
+                self.timeTooEmpty = 0
 
+    def addBall(self):
+        ball = Ball(random.randint(2.5*self.unit_width, 3.5*self.unit_width), 
+                        random.randint(1.5*self.unit_height, 2.5*self.unit_height))
+        self.scene.addItem(ball)
+        
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     
